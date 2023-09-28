c40528dd56b207dcaf75b9096b00162280f04625
Davi <dboscardindossantos@gmail.com>
2023-09-28 17:14:01 -0300

    atualizações não feitas

--- MVC/View/TeleInicial.php
+++ MVC/View/TeleInicial.php
@@ -6,6 +6,27 @@ if(!isset($_SESSION)){
     session_start();
   }
 
+if(isset($_FILES["arquivo"])){
+  $arquivo = $_FILES["arquivo"];
+  
+  if($arquivo['erroe'])
+    die("Falha ao enviar o arquivo");
+  
+   if($arquivo['size'] > 2097152)
+     die("Arquivo muito grande!");
+  
+     $pasta = "arquivos/";
+     $nomeDoArquivo = $arquivo['name'];
+     $novoNomeDoArquivo = uniqid();
+     $extensao = strtolower(pathinfo($nomeDoArquivo, PATHINFO_EXTENSION));
+  
+    if($extensao != "jpg" && $extensao != "png")
+      die("Tipo de arquivo não aceito");
+  
+    $deu_certo = move_uplaod_file();
+  
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +39,14 @@ if(!isset($_SESSION)){
 <body>
     
   Seja bem vindo, <?php echo $_SESSION['nome']; ?>
+
+  <form method="POST" action="">
+
+  <p><label>Selecione um Arquivo</label></p> <br>
+
+  <p><imput name="arquivo" type="file">
+    <buttom name="upload" type="submit">Enviar imagem</buttom>
+  </p>
 
 </body>
 </html>
