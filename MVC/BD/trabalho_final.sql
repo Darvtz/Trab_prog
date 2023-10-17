2b75e9df517566102c992d8a4756c80a45985e90
Davi Boscardin dos Santos <02150154@aluno.canoas.ifrs.edu.br>
2023-10-17 03:34:36 -0300

    Coreções no banco e controller

--- MVC/BD/trabalho_final.sql
+++ MVC/BD/trabalho_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 31-Maio-2023 às 19:52
+-- Tempo de geração: 17-Out-2023 às 08:33
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -24,23 +24,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `papel`
+-- Estrutura da tabela `banidos`
 --
 
-CREATE TABLE `papel` (
-  `id_papel` int(11) NOT NULL,
-  `papel` varchar(60) DEFAULT NULL
+CREATE TABLE `banidos` (
+  `id_banimento` int(11) NOT NULL,
+  `cpf_usuario` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `papel_usuario`
+-- Estrutura da tabela `cargo`
 --
 
-CREATE TABLE `papel_usuario` (
-  `id_usuario` int(11) DEFAULT NULL,
-  `id_papel` int(11) DEFAULT NULL
+CREATE TABLE `cargo` (
+  `id` int(11) NOT NULL,
+  `cargo` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cargo_usuario`
+--
+
+CREATE TABLE `cargo_usuario` (
+  `cpf_usuario` varchar(15) DEFAULT NULL,
+  `id_cargo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postagem_animal`
+--
+
+CREATE TABLE `postagem_animal` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) DEFAULT NULL,
+  `especie` varchar(60) DEFAULT NULL,
+  `raca` varchar(60) DEFAULT NULL,
+  `genero` varchar(60) DEFAULT NULL,
+  `cor` varchar(60) DEFAULT NULL,
+  `ultimo_endereco` varchar(60) DEFAULT NULL,
+  `descricao` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postagem_usuario`
+--
+
+CREATE TABLE `postagem_usuario` (
+  `cpf_usuario` varchar(15) DEFAULT NULL,
+  `id_postagem` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -50,16 +89,14 @@ CREATE TABLE `papel_usuario` (
 --
 
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `email` varchar(60) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
   `nome` varchar(60) DEFAULT NULL,
-  `senha` varchar(60) DEFAULT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(60) NOT NULL,
   `data_nascimento` date DEFAULT NULL,
-  `celular` int(11) DEFAULT NULL,
-  `data_cadastro` timestamp NULL DEFAULT NULL,
-  `sys_whats` tinyint(1) NOT NULL,
-  `sys_email_confirmado` tinyint(1) NOT NULL,
-  `sys_confirmado_termos_de_uso` timestamp NULL DEFAULT NULL
+  `celular` varchar(60) DEFAULT NULL,
+  `data_cadatro` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `foto` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -67,34 +104,67 @@ CREATE TABLE `usuario` (
 --
 
 --
--- Índices para tabela `papel`
+-- Índices para tabela `banidos`
 --
-ALTER TABLE `papel`
-  ADD PRIMARY KEY (`id_papel`);
+ALTER TABLE `banidos`
+  ADD PRIMARY KEY (`id_banimento`),
+  ADD KEY `fk_cpf_usuario_banido` (`cpf_usuario`);
 
 --
--- Índices para tabela `papel_usuario`
+-- Índices para tabela `cargo`
 --
-ALTER TABLE `papel_usuario`
-  ADD KEY `fk_id_usuario` (`id_usuario`),
-  ADD KEY `fk_id_papel` (`id_papel`);
+ALTER TABLE `cargo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `cargo_usuario`
+--
+ALTER TABLE `cargo_usuario`
+  ADD KEY `fk_cpf_usuario_cargo` (`cpf_usuario`),
+  ADD KEY `fk_id_cargo_usuario` (`id_cargo`);
+
+--
+-- Índices para tabela `postagem_animal`
+--
+ALTER TABLE `postagem_animal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `postagem_usuario`
+--
+ALTER TABLE `postagem_usuario`
+  ADD KEY `fk_cpf_usuario_postagem` (`cpf_usuario`),
+  ADD KEY `fk_id_postagem_usuario` (`id_postagem`);
 
 --
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
+  ADD PRIMARY KEY (`cpf`);
 
 --
 -- Restrições para despejos de tabelas
 --
 
 --
--- Limitadores para a tabela `papel_usuario`
+-- Limitadores para a tabela `banidos`
 --
-ALTER TABLE `papel_usuario`
-  ADD CONSTRAINT `fk_id_papel` FOREIGN KEY (`id_papel`) REFERENCES `papel` (`id_papel`),
-  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+ALTER TABLE `banidos`
+  ADD CONSTRAINT `fk_cpf_usuario_banido` FOREIGN KEY (`cpf_usuario`) REFERENCES `usuario` (`cpf`);
+
+--
+-- Limitadores para a tabela `cargo_usuario`
+--
+ALTER TABLE `cargo_usuario`
+  ADD CONSTRAINT `fk_cpf_usuario_cargo` FOREIGN KEY (`cpf_usuario`) REFERENCES `usuario` (`cpf`),
+  ADD CONSTRAINT `fk_id_cargo_usuario` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id`);
+
+--
+-- Limitadores para a tabela `postagem_usuario`
+--
+ALTER TABLE `postagem_usuario`
+  ADD CONSTRAINT `fk_cpf_usuario_postagem` FOREIGN KEY (`cpf_usuario`) REFERENCES `usuario` (`cpf`),
+  ADD CONSTRAINT `fk_id_postagem_usuario` FOREIGN KEY (`id_postagem`) REFERENCES `postagem_animal` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- MVC/Controller/Usuario.php
+++ MVC/Controller/Usuario.php
@@ -4,6 +4,8 @@ $acao = $_GET['acao'];
 include_once '../Model/Usuario.class.php';
 include_once '../Model/Cargo.class.php';
 
+$hash = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
 // Cadastrar no banco
 if($acao='cadastrar'){
     $usuario=new Usuario();
@@ -11,7 +13,7 @@ if($acao='cadastrar'){
     $usuario->setCpf($_POST['cpf']);
     $usuario->setNome($_POST['nome']);
     $usuario->setEmail($_POST['email']);
-    $usuario->serSenha($_POST['senha']);
+    $usuario->setSenha($hash);
     $usuario->setDatanasc($_POST['Datanasc']);
     $usuario->setCelular($_POST['Celular']);
     $usuario->setDatacad(date('Y/m/d'));
