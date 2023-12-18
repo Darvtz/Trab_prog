58bd82ed44135fe13c54bf6209ca4681bf35df4b
davi <dboscardindossantos@gmail.com>
2023-12-18 20:57:49 -0300

    s

--- MVC/Model/Conexao.php
+++ MVC/Model/Conexao.php
@@ -3,7 +3,7 @@ function conexao(){
     try {
     
         $pdo = new PDO('mysql:host=gpetsbd.mysql.dbaas.com.br;dbname=gpetsbd;charset=utf8', 'gpetsbd', 'D@v12004');
-       // $pdo = new PDO('mysql:host=localhost;dbname=trabalho_final; charset=utf8', 'root', '');
+       //$pdo = new PDO('mysql:host=localhost;dbname=trabalho_final; charset=utf8', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
 
